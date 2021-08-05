43029fec4216451120a236091ed782caf666dfe6
TzeSiong <73157643+TzeSiong@users.noreply.github.com>
2021-08-05 15:34:33 +0800

    Updated Level Selector
    Added Level 2 and 3

--- Assets/LevelSelector.cs
+++ Assets/LevelSelector.cs
@@ -8,6 +8,12 @@ public class LevelSelector : MonoBehaviour
     public void LevelOne(){
         SceneManager.LoadScene("Level 1");
     }
+    public void LevelTwo(){
+        SceneManager.LoadScene("Level 2");
+    }
+    public void LevelThree(){
+        SceneManager.LoadScene("Level 3");
+    }
     public void MainMenu(){
         SceneManager.LoadScene("Main Menu");
     }
