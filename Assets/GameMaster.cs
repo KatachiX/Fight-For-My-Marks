91680a66e7d06a8abd106e4d70e684005c2fa340
TzeSiong <73157643+TzeSiong@users.noreply.github.com>
2021-08-08 15:27:28 +0800

    Added Win and Lose Condition

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -4,12 +4,17 @@ using UnityEngine.SceneManagement;
 public class GameMaster : MonoBehaviour {
 
     public static GameMaster gm;
-
     public GameObject enemyBase;
-    public bool WinGame = false;
+    public GameObject allyBase;
+    public GameObject winScreen;
+    public GameObject loseScreen;
+    public static bool WinGame = false;
+    public static bool LoseGame = false;
 
     void Start() 
     {
+        WinGame = false;
+        LoseGame = false;
         if (gm == null)
         {
             gm = this;
@@ -17,13 +22,41 @@ public class GameMaster : MonoBehaviour {
     }
 
     void Update(){
-        if(enemyBase == null){
-            EndLevel();
+        if(enemyBase == null && !WinGame){
+            WinGame = true;
+            GetComponent<PauseMenu>().enabled = false;
+            WinLevel();
+        }
+
+        if(allyBase == null && !LoseGame){
+            LoseGame = true;
+            GetComponent<PauseMenu>().enabled = false;
+            LoseLevel();
         }
     }
 
-    public void EndLevel(){
+    void WinLevel(){
+        winScreen.SetActive(true);
         Time.timeScale = 0.0f;
-        WinGame = true;
+    }
+
+    void LoseLevel(){
+        loseScreen.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void NextLevelButton(){
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //arrange buildindex in order for levels, check if level is 3, hide this button
+    }
+
+    public void TryAgainButton(){
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitLevelButton(){
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Level Selector");
     }
 }
