2ce716795c5a9b2584be8d514d5bc776e5505a86
KatachiX <KatachiX@gmail.com>
2021-08-09 18:23:27 +0800

    Update Tooltips and Spawn UI
    Tooltips format is more uniformed
    Cursor must hover for a while for tooltips to appear to not disrupt gameplay
    Disabled spawn buttons in respective levels

--- Assets/TooltipTrigger.cs
+++ Assets/TooltipTrigger.cs
@@ -5,16 +5,24 @@ using UnityEngine.EventSystems;
 
 public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Multiline()]
     public string content;
     public string header;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        TooltipSystem.Show(content, header);
+        StartCoroutine(DelayShow());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        StopAllCoroutines();
         TooltipSystem.Hide();
+    }
+
+    IEnumerator DelayShow()
+    {
+        yield return new WaitForSeconds(1f);
+        TooltipSystem.Show(content, header);
     }
 }
